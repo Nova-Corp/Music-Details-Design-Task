e90be9df9e7b79d1b11b423afed2c734b6ba557b
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2021-03-10 15:57:00 +0530

    Initial Commit

---
+++ Music-Details-Design-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music-Details-Design-Task
+//
+//  Created by ADMIN on 10/03/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
